139cd2045a197af2122ed27d8503c8fd3a42ebb9
amirw <amir.weinfeld@mail.huji.ac.il>
2023-06-08 13:12:38 +0300

    started task_3

--- project/task_1/code/hackathon_code/learners/task_3.py
+++ project/task_1/code/hackathon_code/learners/task_3.py
@@ -1,1 +1,43 @@
 # Task 1.2.3
+import numpy as np
+import pandas as pd
+from sklearn.decomposition import PCA
+
+from project.task_1.code.hackathon_code.utils.preprocess import create_x_y_df
+
+LABEL_NAME = "cancellation_datetime"
+DATAFRAME_IMPORTANT_COLS = ["guest_is_not_the_customer",
+                            "no_of_adults",
+                            "no_of_children",
+                            "no_of_extra_bed",
+                            "no_of_room"]
+
+
+def person_corr(X: pd.DataFrame, y: np.ndarray) -> list:
+    corr_list = []
+    # calculating correlation for each header
+    for name in X.columns:
+        corr_list.append(np.corrcoef(X[name].values, y)[0, 1])
+    return corr_list
+
+
+def churn_prediction_model(raw_data: pd.DataFrame):
+    X, y = create_x_y_df(raw_data, DATAFRAME_IMPORTANT_COLS, LABEL_NAME)
+
+    # TODO: this part should be done in pre-process!
+    y = np.where(pd.Series(y).isnull(), 0, 1)
+
+    # calculating person correlation:
+    corr_list = person_corr(X, y)
+    print(corr_list, "\n")
+
+    # calculating pca:
+    pca = PCA(n_components=5)
+    pca.fit(X, y)
+    print(pca.explained_variance_)
+    print(pca.components_)
+
+
+
+
+
--- project/task_1/code/main.py
+++ project/task_1/code/main.py
@@ -2,8 +2,10 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 import project.task_1.code.hackathon_code.utils.preprocess as preprocess
-from project.task_1.code.hackathon_code.learners.cancellation_prediction_classifier import \
-    test_classify_cancellation_prediction
+from project.task_1.code.hackathon_code.learners.task_1 import \
+    temp_classify_cancellation_prediction
+from project.task_1.code.hackathon_code.learners.task_3 import \
+    churn_prediction_model
 
 SEED = 420420
 
@@ -16,9 +18,15 @@ DATA_ORIG_PATH = r'../../../instructions/agoda_cancellation_train.csv'
 
 if __name__ == "__main__":
     np.random.seed(SEED)
-    data = preprocess.read_csv_to_dataframe(DATA_25_PATH)
-    # data = preprocess.create_additional_cols(data)
-    train, test = train_test_split(data, test_size=0.2)
+    raw_data = preprocess.read_csv_to_dataframe(DATA_25_PATH)
 
-    test_classify_cancellation_prediction(train, test)
-    # print(data.head(30))
+    # Q1:
+    # temp_classify_cancellation_prediction(raw_data)
+    # classify_cancellation_prediction(raw_data)
+
+    # Q2:
+
+    # Q3:
+    churn_prediction_model(raw_data)
+
+    # Q4:
