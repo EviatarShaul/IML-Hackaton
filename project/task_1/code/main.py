b4edfbca0b940285a7082115fe4fb0e76ad6f6e6
EviatarShaul <64027390+EviatarShaul@users.noreply.github.com>
2023-06-08 16:41:34 +0300

    add main function

--- project/task_1/code/main.py
+++ project/task_1/code/main.py
@@ -1,5 +1,9 @@
+import sys
+
 import numpy as np
+import os
 from sklearn.model_selection import train_test_split
+from argparse import ArgumentParser
 
 import project.task_1.code.hackathon_code.utils.csv_helper
 import project.task_1.code.hackathon_code.utils.preprocess as preprocess
@@ -13,11 +17,12 @@ DATA_75_PATH = r'../../../instructions/divisions/agoda_train_0_from_4.csv'
 DATA_100_PATH = r'../../../instructions/divisions/agoda_train_0_from_4.csv'
 DATA_ORIG_PATH = r'../../../instructions/agoda_cancellation_train.csv'
 
-if __name__ == "__main__":
-    np.random.seed(SEED)
-    data = project.task_1.code.hackathon_code.utils.csv_helper.read_csv_to_dataframe(DATA_25_PATH)
+
+def main(input_file):
+    data = project.task_1.code.hackathon_code.utils.csv_helper.read_csv_to_dataframe(input_file)
     data, default_values = preprocess.generic_preprocess(data)
     temp_classify_cancellation_prediction(data)
+    # todo uncomment before submit
     # # Task 1
     # try:
     #     task_1.code.hackathon_code.learners.task_1.task_1_routine(data)
@@ -38,3 +43,13 @@ if __name__ == "__main__":
     #     task_1.code.hackathon_code.learners.task_4.task_4_routine(data)
     # except Exception as e:
     #     pass
+
+
+if __name__ == "__main__":
+    np.random.seed(SEED)
+    # if len(sys.argv) != 2:
+    #     exit("No input file!")
+    # input_file = sys.argv[1]
+
+    input_file = DATA_25_PATH  # todo delete
+    main(input_file)
