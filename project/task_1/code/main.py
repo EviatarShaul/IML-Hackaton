da9a92f16f6ad3b3728e05633a043f41a0aeb6cd
amirw <amir.weinfeld@mail.huji.ac.il>
2023-06-08 09:34:19 +0300

    added: empty python for learners & setting seed in main

--- project/task_1/code/main.py
+++ project/task_1/code/main.py
@@ -1,4 +1,7 @@
+import numpy as np
 import project.task_1.code.hackathon_code.utils.preprocess as preprocess
+
+SEED = 420420
 
 DATA_25_PATH = r'../../../instructions/divisions/agoda_train_0_from_4.csv'
 DATA_50_PATH = r'../../../instructions/divisions/agoda_train_0_from_4.csv'
@@ -8,6 +11,7 @@ DATA_ORIG_PATH = r'../../../instructions/agoda_cancellation_train.csv'
 
 
 if __name__ == "__main__":
+    np.random.seed(SEED)
     data = preprocess.read_csv_to_dataframe(DATA_25_PATH)
     data = preprocess.create_additional_cols(data)
     print(data.head(30))
