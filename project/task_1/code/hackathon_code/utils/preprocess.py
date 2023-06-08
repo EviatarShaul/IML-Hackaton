e5f64e1c627257ef5cbe347d68010545e81d7e05
itay.braha <itay.braha@mail.huji.ac.il>
2023-06-08 11:07:42 +0300

    Currency conversion added and documented

--- project/task_1/code/hackathon_code/utils/preprocess.py
+++ project/task_1/code/hackathon_code/utils/preprocess.py
@@ -1,7 +1,33 @@
+import datetime
 import pandas as pd
 import csv
 import random
 import os
+from currency_converter import CurrencyConverter
+
+# Initializing a Currency Converter
+currency_converter = CurrencyConverter()
+
+
+def convert_currency_to_usd(amount: float, curr: str, date: datetime.date) -> float:
+    """
+    Converts a given currency to USD based on its value at a given date.
+
+    Parameters:
+    - amount (float): The amount of currency to be converted.
+    - curr (str): The currency code of the currency to be converted.
+    - date (datetime.date): The trade date for which the currency conversion should be performed.
+
+    Returns:
+    - float: The converted amount in USD.
+
+    Note:
+    - This function relies on an external currency converter library named `currency_converter`.
+    - The `currency_converter` library should be installed and imported before using this function as requested
+        the requirements file.
+    """
+
+    return currency_converter.convert(amount=amount, currency=curr, new_currency='USD', date=date)
 
 
 def divide_csv_file(path, divisions, randomize):
